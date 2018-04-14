a3f2dfb26c9e8411e26c1794fd1cae937256bf8f
FKYnJYQ <loveress01@gmail.com>
2018-04-15 01:02:14 +0800

    init

---
+++ new_config.sh
@@ -0,0 +1,84 @@
+# install zsh
+# install plugins
+# install vim
+# install docker
+# install ---
+
+# 检查是否为root
+if [ $USER -t root ]
+return 
+
+
+cat > /etc/yum.repos.d/CentOS-Base.repo <<EOF
+[base]
+name=CentOS-$releasever - Base
+#mirrorlist=http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=os
+baseurl=https://mirrors.ustc.edu.cn/centos/$releasever/os/$basearch/
+gpgcheck=1
+gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7
+
+#released updates
+[updates]
+name=CentOS-$releasever - Updates
+# mirrorlist=http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=updates
+baseurl=https://mirrors.ustc.edu.cn/centos/$releasever/updates/$basearch/
+gpgcheck=1
+gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7
+
+#additional packages that may be useful
+[extras]
+name=CentOS-$releasever - Extras
+# mirrorlist=http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=extras
+baseurl=https://mirrors.ustc.edu.cn/centos/$releasever/extras/$basearch/
+gpgcheck=1
+gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7
+
+#additional packages that extend functionality of existing packages
+[centosplus]
+name=CentOS-$releasever - Plus
+# mirrorlist=http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=centosplus
+baseurl=https://mirrors.ustc.edu.cn/centos/$releasever/centosplus/$basearch/
+gpgcheck=1
+enabled=0
+gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7
+EOF
+sudo yum makecache
+yum update
+
+yum install -y -q zsh vim curl docker axel iftop nload
+
+yum install -y yum-utils \
+           device-mapper-persistent-data \
+           lvm2
+
+curl -fsSL get.docker.com -o get-docker.sh
+sudo sh get-docker.sh --mirror Aliyun
+
+sudo groupadd docker
+sudo usermod -aG docker $USER
+
+systemctl start docker
+docker pull php
+docker pull node
+docker pull nginx
+docker pull mariadb
+docker pull centos
+docker pull phpmyadmin/phpmyadmin
+docker pull kali-linux-docker
+
+
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+cd ~ 
+cat > .zshrc << EOF
+export PATH=$HOME/bin:/usr/local/bin:$PATH
+export ZSH=~/.oh-my-zsh
+ZSH_THEME="random"
+COMPLETION_WAITING_DOTS="true"
+plugins=(
+  git docker
+)
+source $ZSH/oh-my-zsh.sh]
+EOF
+
+
