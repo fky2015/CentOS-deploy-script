7851abe84b0dd36c0c81725783cf7242180e2757
FKYnJYQ <loveress01@gmail.com>
2018-04-15 13:27:35 +0800

    update

--- new_config.sh
+++ new_config.sh
@@ -14,33 +14,33 @@ fi
 
 cat > /etc/yum.repos.d/CentOS-Base.repo <<EOF
 [base]
-name=CentOS-$releasever - Base
-#mirrorlist=http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=os
-baseurl=https://mirrors.ustc.edu.cn/centos/$releasever/os/$basearch/
+name=CentOS-\$releasever - Base
+#mirrorlist=http://mirrorlist.centos.org/?release=\$releasever&arch=\$basearch&repo=os
+baseurl=https://mirrors.ustc.edu.cn/centos/\$releasever/os/\$basearch/
 gpgcheck=1
 gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7
 
 #released updates
 [updates]
-name=CentOS-$releasever - Updates
-# mirrorlist=http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=updates
-baseurl=https://mirrors.ustc.edu.cn/centos/$releasever/updates/$basearch/
+name=CentOS-\$releasever - Updates
+# mirrorlist=http://mirrorlist.centos.org/?release=\$releasever&arch=\$basearch&repo=updates
+baseurl=https://mirrors.ustc.edu.cn/centos/\$releasever/updates/\$basearch/
 gpgcheck=1
 gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7
 
 #additional packages that may be useful
 [extras]
-name=CentOS-$releasever - Extras
-# mirrorlist=http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=extras
-baseurl=https://mirrors.ustc.edu.cn/centos/$releasever/extras/$basearch/
+name=CentOS-\$releasever - Extras
+# mirrorlist=http://mirrorlist.centos.org/?release=\$releasever&arch=\$basearch&repo=extras
+baseurl=https://mirrors.ustc.edu.cn/centos/\$releasever/extras/\$basearch/
 gpgcheck=1
 gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7
 
 #additional packages that extend functionality of existing packages
 [centosplus]
-name=CentOS-$releasever - Plus
-# mirrorlist=http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=centosplus
-baseurl=https://mirrors.ustc.edu.cn/centos/$releasever/centosplus/$basearch/
+name=CentOS-\$releasever - Plus
+# mirrorlist=http://mirrorlist.centos.org/?release=\$releasever&arch=\$basearch&repo=centosplus
+baseurl=https://mirrors.ustc.edu.cn/centos/\$releasever/centosplus/\$basearch/
 gpgcheck=1
 enabled=0
 gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7
@@ -74,14 +74,14 @@ docker pull kali-linux-docker
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 cd ~ 
 cat > .zshrc << EOF
-export PATH=$HOME/bin:/usr/local/bin:$PATH
+export PATH=\$HOME/bin:/usr/local/bin:\$PATH
 export ZSH=~/.oh-my-zsh
 ZSH_THEME="random"
 COMPLETION_WAITING_DOTS="true"
 plugins=(
   git docker
 )
-source $ZSH/oh-my-zsh.sh]
+source \$ZSH/oh-my-zsh.sh]
 EOF
 
 
